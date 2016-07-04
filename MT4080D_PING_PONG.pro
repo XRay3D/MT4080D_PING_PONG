9a52fce7f398707708c0f92baa9586b77f8f93b5
XRay3D <xray3d@ya.ru>
2016-07-04 22:41:40 +0400

    1

--- MT4080D_PING_PONG.pro
+++ MT4080D_PING_PONG.pro
@@ -8,7 +8,7 @@ QT       += core gui serialport
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
-TARGET = untitled6
+TARGET = MT4080D_PING_PONG
 TEMPLATE = app
 
 
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -47,45 +47,56 @@ void MainWindow::on_comboBox_2_currentIndexChanged(const QString& arg1)
     port.setParity(QSerialPort::NoParity);
     port.setFlowControl(QSerialPort::NoFlowControl);
     port.setPortName(arg1);
-    timerId = startTimer(100);
+
+    if (!port.open(TESTER::WriteOnly)) {
+        qDebug() << "open err";
+    }
+    else {
+        timerId = startTimer(100);
+    }
 }
 
 void MainWindow::on_comboBox_currentIndexChanged(const QString& arg1)
 {
     qDebug() << "comboBox" << arg1;
     rele.close();
-    rele.setPortName("COM5");
-    if (!rele.open(TESTER::WriteOnly))
+    rele.setPortName(arg1);
+    if (!rele.open(TESTER::ReadWrite))
         qDebug() << "open err";
 }
 
 void MainWindow::writeSettings()
 {
-    QSettings settings("Moose Soft", "Clipper");
+    QSettings settings("XrSoft", "MT4080D_PING_PONG");
 
     settings.beginGroup("MainWindow");
     settings.setValue("size", size());
     settings.setValue("pos", pos());
     settings.setValue("comboBox", ui->comboBox->currentIndex());
     settings.setValue("comboBox_2", ui->comboBox_2->currentIndex());
+    settings.setValue("doubleSpinBox", ui->doubleSpinBox->value());
+    settings.setValue("doubleSpinBox_2", ui->doubleSpinBox_2->value());
     settings.endGroup();
 }
 
 void MainWindow::readSettings()
 {
-    QSettings settings("Moose Soft", "Clipper");
+    QSettings settings("XrSoft", "MT4080D_PING_PONG");
 
     settings.beginGroup("MainWindow");
     resize(settings.value("size", QSize(400, 400)).toSize());
     move(settings.value("pos", QPoint(200, 200)).toPoint());
     ui->comboBox->setCurrentIndex(settings.value("comboBox").toInt());
     ui->comboBox_2->setCurrentIndex(settings.value("comboBox_2").toInt());
+    ui->doubleSpinBox->setValue(settings.value("doubleSpinBox").toDouble());
+    ui->doubleSpinBox_2->setValue(settings.value("doubleSpinBox_2").toDouble());
     settings.endGroup();
 }
 
 void MainWindow::timerEvent(QTimerEvent* TimerEvent)
 {
     QByteArray data;
+    int tmp = 0;
     float val = ui->doubleSpinBox->value() + ui->doubleSpinBox_2->value() * (float(qrand()) / RAND_MAX);
     ui->lineEdit_2->setText(QString("%1").arg(val));
 
@@ -93,7 +104,11 @@ void MainWindow::timerEvent(QTimerEvent* TimerEvent)
         data.clear();
         data.append(2);
         data.append(1);
-
+        data.append(QByteArray().fromRawData((char*)&val, 4));
+        foreach (char var, data) {
+            tmp += var;
+        }
+        data.append(256 - (tmp & 0xFF));
         if (port.isOpen()) {
             port.write(data);
         }
